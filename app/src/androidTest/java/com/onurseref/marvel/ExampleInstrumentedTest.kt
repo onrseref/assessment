c114754dc343cebe859dce2ff3ead96433457c0c
Onur <onur.seref@loodos.com>
2020-09-30 15:15:24 +0300

    completed

--- app/src/androidTest/java/com/onurseref/marvel/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/onurseref/marvel/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.onurseref.mackolik", appContext.packageName)
+        assertEquals("com.onurseref.marvel", appContext.packageName)
     }
 }
