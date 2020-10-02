ee886cad9fcd540a3f4ee9c95b4eb720548b0744
Onur <onur.seref@loodos.com>
2020-10-02 09:57:49 +0300

    characters added

---
+++ app/src/main/java/com/onurseref/marvel/base/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.onurseref.marvel.base
+
+import android.os.Bundle
+import dagger.android.support.DaggerAppCompatActivity
+
+abstract class BaseActivity : DaggerAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindScreen()
+    }
+
+    abstract fun bindScreen()
+}
